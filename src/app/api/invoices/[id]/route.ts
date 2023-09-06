4090119d3191c43d48fe9333d72d0f97adb028b2
shambutt247 <ihtishambutt247@gmail.com>
2023-09-06 01:56:01 +0100

    added sort api through query params

--- src/app/api/invoices/[id]/route.ts
+++ src/app/api/invoices/[id]/route.ts
@@ -1,17 +1,33 @@
-import { NextResponse } from 'next/server'
-import invoiceData from '@/data/invoices.json';
+import { NextResponse, NextRequest } from "next/server";
+import invoiceData from "@/data/invoices.json";
 
 interface ParamType {
   id: string;
 }
 
 interface ContextType {
-  params: ParamType
+  params: ParamType;
 }
 
-export async function GET(request: Request, context: ContextType) {
-  const data = invoiceData.invoices
-    .filter(({recipient}) => recipient === context.params.id );
+function invoiceAsc(a: any, b: any) {
+  return a.number - b.number;
+}
 
-  return NextResponse.json({ data })
+function invoiceDsc(a: any, b: any) {
+  return b.number - a.number;
+}
+
+export async function GET(request: NextRequest, context: ContextType) {
+  const sortByQuery = request.nextUrl.searchParams.get("sortBy");
+
+  const data = invoiceData.invoices.filter(
+    ({ recipient }) => recipient === context.params.id
+  );
+
+  if (sortByQuery && sortByQuery === "asc") {
+    data.sort(invoiceAsc);
+  } else if (sortByQuery && sortByQuery === "dsc") {
+    data.sort(invoiceDsc);
+  }
+  return NextResponse.json({ data });
 }
